45de7ac141d0571f0bdc4ef0ea3f1a66f77f669b
khcastrillon <kh.castrillon@uniandes.edu.co>
2022-05-01 19:44:43 -0500

    URLs

--- PredictionModel.py
+++ PredictionModel.py
@@ -3,8 +3,12 @@ from joblib import load
 class Model:
 
     def __init__(self,columns):
-        self.model = load("assets/modelo.joblib")
+        self.model = load("assets/pipeline.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
         return result
+
+    def R2(self, data, y):
+        r2 = self.model.score(data,y)
+        return r2
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from typing import Optional
 import pandas as pd
-from joblib import load
+# from joblib import load
 from fastapi import FastAPI
 from DataModel import DataModel
+import PredictionModel
 
 app = FastAPI()
 
@@ -18,6 +19,20 @@ def read_item(item_id: int, q: Optional[str] = None):
 def make_predictions(dataModel: DataModel):
     df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
     df.columns = dataModel.columns()
-    model = load("assets/modelo.joblib")
-    result = model.predict(df)
+    X = df.drop('Life expectancy', axis=1)
+    y = df['Life expectancy']
+    # model = load("assets/pipeline.joblib")
+    result = PredictionModel.Model.make_predictions(X)
+    # result = model.predict(df)
+    return result
+
+@app.post("/predictR2")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    X = df.drop('Life expectancy', axis=1)
+    y = df['Life expectancy']
+    # model = load("assets/pipeline.joblib")
+    result = PredictionModel.Model.R2(X, y)
+    # result = model.predict(df)
     return result
